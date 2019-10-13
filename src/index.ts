4ca663af0b5f48ae79577152fa454316d88151ca
Gevorg A. Galstyan <gevorg@galstyan.am>
2019-10-13 20:59:50 +0400

    fix: run bin on a js file

--- src/index.ts
+++ src/index.ts
@@ -1,4 +1,17 @@
 (async () => {
+  // Clean the console
   // tslint:disable-next-line: no-console
   console.clear();
+
+  // Reset the store
+
+  // Read the CLI args
+
+  // Store the CLI args in the store
+
+  // Select the action
+
+  // Configure based on action
+
+  // Launch the action
 })();
