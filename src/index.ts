6088bbe74ffb4f3c016ddb5befd037878b71c0fa
Gevorg A. Galstyan <gevorg@galstyan.am>
2019-10-20 12:30:22 +0400

    fix: set max_old_space_size runtime

--- src/index.ts
+++ src/index.ts
@@ -1,10 +1,13 @@
-#!/usr/bin/env node
+#!/usr/bin/env node --max_old_space_size=4096
+import v8 from "v8";
 
 import { configureActionAndEnv } from "./actions";
 import { configureDB } from "./dynamodb";
 import { initProcess } from "./process";
 
 (async () => {
+  v8.setFlagsFromString("--max_old_space_size=4096");
+
   console.clear();
 
   await configureActionAndEnv();
