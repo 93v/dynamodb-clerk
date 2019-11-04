21664b4c041cb2ade08ca350d1a7fde833fd741d
Gevorg A. Galstyan <gevorg@galstyan.am>
2019-11-05 02:23:59 +0400

    feat: remove version

--- src/index.ts
+++ src/index.ts
@@ -1,17 +1,10 @@
 #!/usr/bin/env node --max_old_space_size=4096
 import v8 from "v8";
-import { argv } from "yargs";
-import pkg from "../package.json";
 import { configureActionAndEnv } from "./actions";
 import { configureDB } from "./dynamodb";
 import { initProcess } from "./process";
 
 (async () => {
-  if (argv.version != null) {
-    console.log(`${pkg.name} version ${pkg.version}`);
-    return;
-  }
-
   v8.setFlagsFromString("--max_old_space_size=4096");
 
   console.clear();
