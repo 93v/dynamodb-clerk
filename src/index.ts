7a18cd9350ec31c62586540b0300a83e4e7bbfe7
Gevorg A. Galstyan <gevorg@galstyan.am>
2020-11-04 17:16:49 +0400

    fix: do not set env max old flags

--- src/index.ts
+++ src/index.ts
@@ -1,14 +1,10 @@
 #!/usr/bin/env node
-// #!/usr/bin/env node --max_old_space_size=4096
-// import v8 from "v8";
 
 import { configureActionAndEnv } from "./actions";
 import { configureDB } from "./dynamodb";
 import { initProcess } from "./process";
 
 (async () => {
-  // v8.setFlagsFromString("--max_old_space_size=4096");
-
   console.clear();
 
   process.env.AWS_NODEJS_CONNECTION_REUSE_ENABLED = "1";
